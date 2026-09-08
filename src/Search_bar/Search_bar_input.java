566d557c3672ab2201224f82f461e246be2f9b55
akashkr1290 <akashkr129012@gmail.com>
2026-09-08 16:59:15 +0000

    creating demo test of search_bar

--- src/Search_bar/Search_bar_input.java
+++ src/Search_bar/Search_bar_input.java
@@ -1,5 +1,6 @@
 package Search_bar;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Search_bar_input {
 	public static void main(String args[]) {
@@ -7,6 +8,11 @@ public class Search_bar_input {
 		
 		dr.get("https://www.google.com");
 		dr.navigate().to("https://www.wikipedia.org");
+		dr.navigate().refresh();
+		
+		dr.findElement(By.xpath("//input[@id ='searchInput']")).sendKeys("Amazon");
+		dr.findElement(By.xpath("//button[@type='submit']")).click();
+		
 	}
 
 }
