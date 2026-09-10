020c20ca224115dab28a04ec5585a31e923057d6
akashkr1290 <akashkr129012@gmail.com>
2026-09-10 13:08:54 +0000

    fixed Article_Title_Test.java

--- src/Article_Testing/Article_Title_Test.java
+++ src/Article_Testing/Article_Title_Test.java
@@ -11,8 +11,15 @@ ChromeDriver dr = new ChromeDriver();
 		
 		dr.get("https://www.google.com");
 		dr.navigate().to("https://www.wikipedia.org");
+		
+		try {
+			dr.findElement(By.xpath("//button[@aria-label='minimize']")).click();
+			dr.findElement(By.xpath("//div[@aria-label='close']")).click();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 		dr.findElement(By.xpath("//input[@id = 'searchInput']")).sendKeys("India");
-		dr.findElement(By.xpath("//button[@type='submit']")).click();
+		dr.findElement(By.xpath("//button[@type='submit']/child::i")).click();
 		String title = dr.findElement(By.xpath("//span[@class = 'mw-page-title-main']")).getText();
 		System.out.println("Article Title : "+title);
 		if(title.equals("India")) {
