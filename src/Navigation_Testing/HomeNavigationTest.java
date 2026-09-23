2a8f2defe8647f9572445a64de6399b09f5f9816
akashkr1290 <akashkr129012@gmail.com>
2026-09-23 16:03:47 +0000

    adding HomeNavigation Test and there Excel File

--- src/Navigation_Testing/HomeNavigationTest.java
+++ src/Navigation_Testing/HomeNavigationTest.java
@@ -7,7 +7,32 @@ public class HomeNavigationTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ChromeDriver dr = new ChromeDriver();
 		
+		dr.get("https://www.google.com");
+		dr.navigate().to("https://www.wikipedia.org");
+		
+		if(dr.findElement(By.xpath("//body[@id='www-wikipedia-org']")).isDisplayed()) {
+			System.out.println("Home Page is Displaying");
+			
+		}
+		else {
+			System.out.println("Home Page is not Displaying.");
+		}
+		
+		if(dr.findElement(By.xpath("//input[@id='searchInput']")).isDisplayed()) {
+			System.out.println("Search bar is Displayed.");
+		}else {
+			System.out.println("Serach bar is not Displaying");
+		}
+		
+		if(dr.findElement(By.xpath("//button[@type='submit']")).isDisplayed()) {
+			System.out.println("Search Button is Displaying.");
+		}else {
+			System.out.println("Search Button is not Displaying.");
+		}
+		
+		dr.quit();
 
 	}
 
